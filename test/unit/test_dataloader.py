c41545b0dd428e6b952bfa443d6b41e61f866bb9
Oscar <obrask1@volvocars.com>
2023-05-30 16:05:44 +0200

    :remove global config mocking

--- test/unit/test_dataloader.py
+++ test/unit/test_dataloader.py
@@ -5,42 +5,24 @@ Unit test of ZOD dataset loader and partitioner.
 from logging import INFO
 import torch
 import pytest
-from dataclasses import dataclass
+
 from edge_code.data_loader import load_datasets
 from server_code.data_partitioner import partition_train_data
 from common.static_params import PartitionStrategy
 from common.logger import fleet_log
+from common.static_params import global_configs
+
 
 @pytest.fixture()
-def global_configs():
-    @dataclass
-    class GlobalConfigs:
-        def __init__(self):
-            self.SERVER_MAIN_PATH = "/root/Fleet/oscar/fleet-learning/"
-            self.TARGET_DISTANCES = [5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 95, 110, 125, 145, 165]
-            self.NUM_CLIENTS = 1000
-            self.PERCENTAGE_OF_DATA = 0.02
-            self.NUM_OUTPUT = 51
-            self.IMG_SIZE = 256
-            self.RUN_PRETRAINED = False
-            self.BATCH_SIZE = 8
-            self.VAL_FACTOR = 0.1  # percentage of train data to use for validation
-            self.SUBSET_FACTOR = 0.003  # subset of test frames to use
-            self.OUTPUT_SIZE = 66
-            self.STORED_GROUND_TRUTH_PATH = "/mnt/ZOD/ground_truth.json"
-            self.DATASET_ROOT = "/mnt/ZOD"
-
-    return GlobalConfigs()
+def n_clients():
+    return 100
 
 
-def test_dataloader(mocker, global_configs):
-
-    # mock global params
-    mocker.patch("common.static_params.GlobalConfigs", return_value=global_configs)
+def test_dataloader(n_clients):
 
     # partition data among clients
     partitions = partition_train_data(
-        PartitionStrategy.RANDOM, global_configs.NUM_CLIENTS
+        PartitionStrategy.RANDOM, n_clients
     )
     fleet_log(INFO, partitions['0'][:5])
     fleet_log(INFO, partitions['1'][:5])
@@ -51,7 +33,7 @@ def test_dataloader(mocker, global_configs):
     fleet_log(INFO, type(train_loader))
 
     # checks
-    assert len(partitions.keys()) == global_configs.NUM_CLIENTS
+    assert len(partitions.keys()) == n_clients
 
     train_data, train_targets = next(iter(train_loader))
     val_data, val_targets = next(iter(val_loader))
