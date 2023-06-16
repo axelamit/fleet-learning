0a966e803dcf5a64aad4a57ec4f5fc9cb7921c1b
Oscar <obrask1@volvocars.com>
2023-06-16 14:58:57 +0200

    add visible buffer to handle queue

--- communication/scheduler/scheduler.py
+++ communication/scheduler/scheduler.py
@@ -1,7 +1,7 @@
 import socket
-from _thread import start_new_thread
 import threading
 import time
+import select
 
 
 class AGX:
@@ -15,6 +15,7 @@ class AGX:
         self.BUFFER = []
         self.CLIENT_ID = 0
         self.MAX_BUFFER_SIZE = 100
+        self.TIMEOUT = 1
 
     def run_scheduler(self):
         self.print_lock = threading.Lock()
@@ -22,34 +23,49 @@ class AGX:
 
         try:
             while True:
-                #self._update_que()
-                self._process_virtual_vehicle_connections()
+                self._update_queue()
+                self._process_queue()
 
         except KeyboardInterrupt as e:
             print("Keyboard interrupt")
-            self.s.close()
+            self.socket.close()
             print("Socket closed.")
 
     def _open_socket(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.s.bind(self.ADDR)
-        self.s.listen(self.MAX_BUFFER_SIZE)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.ADDR)
+        self.socket.listen(self.MAX_BUFFER_SIZE)
         print("[AGX] AGX started")
         print(f"[AGX] Listening on {self.AGX_IP} port {self.PORT}\n")
 
-    def _process_virtual_vehicle_connections(self):
-        conn, addr = self.s.accept()
-        new_virtual_vehicle = (conn, addr)
-        self.CLIENT_ID += 1
-        print(f"[AGX] Processing virtual vehicle ID {self.CLIENT_ID}")
-        self.process_virtual_vehicle(new_virtual_vehicle)
+    def _update_queue(self):
+        while True:
+            # use select to determine readable sockets
+            readable_sockets, _, _ = select.select([self.socket], [], [], self.TIMEOUT)
 
-    def process_virtual_vehicle(self, connected_virtual_vehicle):
-        self.agx_task(connected_virtual_vehicle)
+            if readable_sockets:
+                for readable_socket in readable_sockets:
+                    client_socket, client_address = readable_socket.accept()
 
-    def agx_task(self, connected_virtual_vehicle):
-        conn, _ = connected_virtual_vehicle
+                    # add the client socket to the list of connected virtual vehicles
+                    self.BUFFER.append((client_socket, client_address))
+                    print(f"[AGX] Adding client {client_address} to que")
+            else:
+                break  # if timeout
+
+    def _process_queue(self):
+        if self.BUFFER:
+            print(f"[AGX] Number of connected vehicles in qeue: {len(self.BUFFER)}")
+
+            client_socket, client_address = self.BUFFER.pop(0)
+            new_virtual_vehicle = (client_socket, client_address)
+            self.CLIENT_ID += 1
+            print(f"[AGX] Processing virtual vehicle {self.CLIENT_ID}: {client_address}")
+            self._process_virtual_vehicle(new_virtual_vehicle)
+
+    def _process_virtual_vehicle(self, virtual_vehicle):
+        conn, _ = virtual_vehicle
 
         while True:
             data = conn.recv(self.MSG_LENGTH)
@@ -59,7 +75,7 @@ class AGX:
                 break
 
             stop_session = self._parse_virtual_vehicle_message(
-                connected_virtual_vehicle, data
+                virtual_vehicle, data
             )
 
             if stop_session:
@@ -67,7 +83,6 @@ class AGX:
                 break
 
         conn.close()
-        self.READY = True
 
     def _parse_virtual_vehicle_message(self, connected_virtual_vehicle, data):
 
@@ -77,14 +92,14 @@ class AGX:
 
         if virtual_vehicle_message == self.message_from_virtual_vehicle[0]:
             print(f"[AGX] Received msg from virtual vehicle: {virtual_vehicle_message}")
-            print("[AGX] Scheduling task on AGX")
             conn.sendall(self.message_to_virtual_vehicle[0].encode("utf-8"))
 
         elif virtual_vehicle_message == self.message_from_virtual_vehicle[1]:
+            print(f"[AGX] Received msg from virtual vehicle: {virtual_vehicle_message}")
             print("[AGX] Training started")
             self._train()
             print("[AGX] Training completed")
-            print(f"[AGX] Ending session with vv on {addr[0]}.")
+            print(f"[AGX] Ending session with vehicle {addr}.")
             conn.sendall(self.message_to_virtual_vehicle[1].encode("utf-8"))
             stop_session = True
 
